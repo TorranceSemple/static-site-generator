1d8586272049d2e7b34bf9e5501cdb826c9997e2
Torrance Semple <torrancesemple@gmail.com>
2025-08-18 20:47:26 +0700

    leafnode

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,12 +1,19 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_props_to_HTML(self):
         node = HTMLNode(tag="a", value="Click here", props={"href": "https://www.google.com", "target": "_blank"})
         self.assertEqual(node.props_to_html(),' href="https://www.google.com" target="_blank"')
         self.assertNotEqual(node.props_to_html(),'href="https://www.google.com" target="_blank"')
+        
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        node2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node2.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
     
 if __name__ == "__main__":
     unittest.main()
