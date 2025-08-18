29d0debcff8a572f9f8b561a65db8be2db732031
Torrance Semple <torrancesemple@gmail.com>
2025-08-18 19:40:27 +0700

    test html nodes

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -3,4 +3,10 @@ import unittest
 from htmlnode import HTMLNode
 
 class TestHTMLNode(unittest.TestCase):
-    pass
+    def test_props_to_HTML(self):
+        node = HTMLNode(tag="a", value="Click here", props={"href": "https://www.google.com", "target": "_blank"})
+        self.assertEqual(node.props_to_html(),' href="https://www.google.com" target="_blank"')
+        self.assertNotEqual(node.props_to_html(),'href="https://www.google.com" target="_blank"')
+    
+if __name__ == "__main__":
+    unittest.main()
